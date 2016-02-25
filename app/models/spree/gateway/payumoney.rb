4e8807256c324dda1795829d2a4fc88f82aa9430
Santosh Singh <santosh@antstack.com>
2016-02-25 20:03:25 +0530

    payment confirm page. checksum and amount matched. @todo: mark payment as paid

--- app/models/spree/gateway/payumoney.rb
+++ app/models/spree/gateway/payumoney.rb
@@ -4,12 +4,11 @@ module Spree
     preference :merchant_id, :string
     preference :secret_key, :string
 
-    #attr_accessible :merchant_id, :secret_key
       
     def provider_class
       ::OffsitePayments.integration('Payu_In')
     end
-    
+
     def provider
       #assign payment mode
       OffsitePayments.mode = preferred_test_mode == true ? :test : :production
@@ -19,25 +18,45 @@ module Spree
     def checksum(items)
       provider_class.checksum(preferred_merchant_id, preferred_secret_key, items)
     end    
-    
+
     def auto_capture?
       true
     end
-    
+
     def method_type
       "payumoney"
     end
-    
+
     def support?(source)
       true
     end
-    
+
     def authorization
       self
     end
-    
+
+    def purchase(amount, source, gateway_options={})
+      ActiveMerchant::Billing::Response.new(true, "payumoney success")
+    end
+
+    def success?
+      true
+    end
+
     def txnid(order)
       order.id.to_s + order.number.to_s
+    end
+
+    def service_provider
+      "payu_paisa"
+    end
+
+    def checksum_ok?(itms, pg_hash)
+      Digest::SHA512.hexdigest([preferred_secret_key, *itms, preferred_merchant_id].join("|")) == pg_hash
+    end
+
+    def amount_ok?(order_total, pg_amount)
+      BigDecimal.new(pg_amount) == order_total
     end
   end
 end
